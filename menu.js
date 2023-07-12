78c017efdfc2912c6462c7fdd21e0f99c73014b6
andres <andres@osf.com.mx>
2023-07-12 05:47:34 -0600

    Cambio a items

---
+++ menu.js
@@ -0,0 +1,35 @@
+var menu = {
+    "Desayunos": [
+        {
+            "name": "Omelette",
+            "desc": "Bacon-Pollo-Chorizo-Vegetables",
+            "price": "$12.50"
+        },
+        {
+            "name": "Pancakes",
+            "desc": "3pz",
+            "price": "$8.50"
+        },
+        {
+            "name": "Huevos rancheros",
+            "desc": "",
+            "price": "$10.25"
+        }
+    ],
+    "Tacos":[],
+    "Burritos":[],
+    "Tortas":[],
+    "Hamburguesas":[],
+    "Birria":[],
+    "Quesadillas":[],
+    "Caldos":[],
+    "Mariscos":[],
+    "Platillos":[],
+    "Ensaladas":[],
+    "Nachos":[],
+    "Alitas":[],
+    "Para Niños":[],
+    "Soda":[],
+    "Bebidas Alc.":[],
+    "Postres":[]
+}
