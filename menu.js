6d1a80c44c94c04fcdecaf78bc48514cf0323067
andres <andres@osf.com.mx>
2023-07-13 09:52:27 -0600

    Agregado elementos de menu

--- menu.js
+++ menu.js
@@ -1,34 +1,88 @@
 var menu = {
     "Desayunos": [
-        {
-            "name": "Omelette",
-            "desc": "Bacon-Pollo-Chorizo-Vegetables",
-            "price": "$12.50"
-        },
-        {
-            "name": "Pancakes",
-            "desc": "3pz",
-            "price": "$8.50"
-        },
-        {
-            "name": "Huevos rancheros",
-            "desc": "",
-            "price": "$10.25"
-        }
+        { name: "Omelets", price: "$12.50"},
+        { name: "Pancakes", price: "$10.75", },
+        { name: "Huevos Rancheros", price: "$10.75", },
+        { name: "Huevos a la Mexicana", price: "$10.75", },
+        { name: "Huevos con Chorizo", price: "$10.75", },
+        { name: "Chilaquiles Verdes o Rojos", price: "$16.50", desc: "Bistec, Chorizo o Bacon" },
     ],
-    "Tacos":[],
-    "Burritos":[],
-    "Tortas":[],
-    "Hamburguesas":[],
-    "Birria":[],
-    "Quesadillas":[],
-    "Caldos":[],
+    "Tacos":[
+        { name: "Tacos de Birria", price: "$2.50", desc: "Cebolla, cilantro, guacamole, salsas" },
+        { name: "Tacos de Asada", price: "$2.50", desc: "Cebolla, cilantro, guacamole, salsas" },
+        { name: "Tacos de Pastor", price: "$2.50", desc: "Cebolla, cilantro, guacamole, salsas" },
+        { name: "Tacos de Pollo", price: "$2.50", desc: "Cebolla, cilantro, guacamole, salsas" },
+        { name: "Tacos de Carnitas", price: "$2.50", desc: "Cebolla, cilantro, guacamole, salsas" },
+        { name: "Tacos de Camaron", price: "$3.50", desc: "Cebolla, cilantro, guacamole, salsas" },
+        { name: "Tacos de Pescado", price: "$3.00", desc: "Cebolla, cilantro, guacamole, salsas" },
+
+        { name: "Charola de Tacos de Birria", price: "$40.00", desc: "Cebolla, cilantro, guacamole, salsas" },
+        { name: "Charola de Tacos de Asada", price: "$40.00", desc: "Cebolla, cilantro, guacamole, salsas" },
+        { name: "Charola de Tacos de Pastor", price: "$40.00", desc: "Cebolla, cilantro, guacamole, salsas" },
+        { name: "Charola de Tacos de Pollo", price: "$40.00", desc: "Cebolla, cilantro, guacamole, salsas" },
+        { name: "Charola de Tacos de Carnitas", price: "$40.00", desc: "Cebolla, cilantro, guacamole, salsas" },
+        { name: "Charola de Tacos de Camaron", price: "$48.00", desc: "Cebolla, cilantro, guacamole, salsas" },
+        { name: "Charola de Tacos de Pescado", price: "$48.00", desc: "Cebolla, cilantro, guacamole, salsas" },
+    ],
+    "Burritos":[
+        { name: "Burrito mojado", price: "$11.99", desc: "Servido beans, arroz, cilantro, cebolla, salsa roja o verde"},
+        { name: "Birria", price: "$8.50",},
+        { name: "Asada", price: "$8.50",},
+        { name: "Pastor", price: "$8.50",},
+        { name: "Pollo", price: "$8.50",},
+        { name: "Carnitas", price: "$8.50",},
+        { name: "Camaron", price: "$11.50",},
+    ],
+    "Tortas":[
+        { name: "Birria", price: "$8.25", desc: "Avocado, Lechuga, Tomato, Mayonesa, Frijoles"},
+        { name: "Asada", price: "$8.25", desc: "Avocado, Lechuga, Tomato, Mayonesa, Frijoles"},
+        { name: "Pastor", price: "$8.25", desc: "Avocado, Lechuga, Tomato, Mayonesa, Frijoles"},
+        { name: "Pollo", price: "$8.25", desc: "Avocado, Lechuga, Tomato, Mayonesa, Frijoles"},
+        { name: "Carnitas", price: "$8.25", desc: "Avocado, Lechuga, Tomato, Mayonesa, Frijoles"},
+    ],
+    "Hamburguesas":[
+        { name: "Hamburguesa de res", price: "$10.75"},
+        { name: "Hamburguesa de pollo", price: "$9.75"},
+        { name: "Bacon Burguer", price: "$10.75"},
+        { name: "BBQ Burguer", price: "$10.75"}
+    ],
+    "Sides":[
+        {name: "Tortilla", price: "$1.25" },
+        {name: "Crema", price: "$1.50" },
+        {name: "Queso", price: "$2.50" },
+        {name: "Arroz con frijoles", price: "$3.00" },
+        {name: "Chips en salsa", price: "$3.00" },
+        {name: "Chiles toreados", price: "$2.00" },
+    ],
+    "Birria":[
+        {name: "Birria de Chivo",price: "$16.99", desc: "Con Tortillas" },
+        {name: "Birria de Res",price: "$15.99", desc: "Con Tortillas" },
+    ],
     "Mariscos":[],
     "Platillos":[],
-    "Ensaladas":[],
+    "Caldos":[
+        {name: "Menudo",price: "$11.95" },
+        {name: "Albondiga Caldo de Res",price: "$12.50" },
+        {name: "Caldo de Pollo",price: "$12.50" },
+        {name: "Pozole",price: "$12.50" }
+    ],
+    "Ensaladas":[
+        {name: "Ensalada de camarón", price: "$13.99" },
+        {name: "Chicken Cesar Salad", price: "$12.99" },
+        {name: "Garden Salad", price: "$7.99" },
+        {name: "Ensalada con pollo", price: "$8.99" }
+    ],
     "Nachos":[],
     "Alitas":[],
-    "Para Niños":[],
+    "Para Niños":[
+        { name: "Taquitos dorados", price: "$5.00", desc: "Servido con Arroz y Frijoles"},
+        { name: "Flautas de pollo", price: "$5.50", desc: "Servido con Arroz y Frijoles"},
+        { name: "Enchilada", price: "$4.50", desc: "Servido con Arroz y Frijoles"},
+        { name: "Quesadilla", price: "$3.50", desc: "Servido con Arroz y Frijoles"},
+        { name: "Papas fritas", price: "$4.00", desc: "Servido con Arroz y Frijoles"},
+        { name: "Chicken tender", price: "$5.00", desc: "Servido con Arroz y Frijoles"},
+        { name: "Hamburguesa con papas", price: "$8.25", desc: "Servido con Arroz y Frijoles"},       
+    ],
     "Soda":[],
     "Bebidas Alc.":[],
     "Postres":[]
